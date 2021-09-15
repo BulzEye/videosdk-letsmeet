588fb74b3391618e060e7724ca5a44f351d748dc
Abhinav Yadav <actcrick02@gmail.com>
2021-09-15 18:35:37 +0530

    home page

--- server.js
+++ server.js
@@ -23,55 +23,6 @@ app.get("/get-token", (req, res) => {
   console.log(token);
 });
 
-
- 
-  
-    app.route("/")
-      .get((req, res) => {
-           res.render('home');
-      });
-  
-  
-  app.route("/main")
-  .get((req, res) => {
-    res.render('main');
-  });
-  
-  
-  app.route("/meet")
-  .get((req, res) => {
-    const API_KEY = process.env.ZUJONOW_API_KEY;
-    const SECRET_KEY = process.env.ZUJONOW_SECRET_KEY;
-    const option = { expiresIn: "10m", algorithm: "HS256" };
-    const payload = {
-      apikey: API_KEY,
-    };
-    let token = jwt.sign(payload, SECRET_KEY, option);
-  
-    console.log(token);
-  
-    
-  
-    var options = {
-      method: "POST",
-      url: "https://api.zujonow.com/v1/meetings",
-      headers: {
-        Authorization: `${token}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    };
-    
-    request(url, options)
-      .then((res) => {
-        res.json()})
-      .then((json) => {
-        console.log(json)})
-      .catch((err) => console.error("error:" + err));
-      
-    
-    res.redirect("/main")
-  });
   
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
